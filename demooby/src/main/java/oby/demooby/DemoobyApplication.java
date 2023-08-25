692c5335a4a7d11df43dc61dae2a632d312cbc8d
kennedy <ezebuilokennedy@gmail.com>
2023-08-25 19:23:03 +0100

    first commit

---
+++ demooby/src/main/java/oby/demooby/DemoobyApplication.java
@@ -0,0 +1,13 @@
+package oby.demooby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoobyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoobyApplication.class, args);
+	}
+
+}
---
+++ demooby/src/test/java/oby/demooby/DemoobyApplicationTests.java
@@ -0,0 +1,13 @@
+package oby.demooby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoobyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
